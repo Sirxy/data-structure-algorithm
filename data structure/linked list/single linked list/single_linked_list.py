056a747e682434ff96dbbf806fc62dcb249d88fe
xuyang <203468558@qq.com>
2020-04-29 18:39:31 +0800

    add single_linked_list.py's content,and so on.

--- data structure/linked list/single linked list/single_linked_list.py
+++ data structure/linked list/single linked list/single_linked_list.py
@@ -47,17 +47,18 @@ class SingleLinkList(object):
 		while __cur is not None:  # Thinking:循环的结束条件是 __cur.next is not None 还是 __cur is not None?
 			print(__cur.element, end=" ")
 			__cur = __cur.next
+		print()
 
-	def add(self, item):
+	def add(self, elem):
 		"""在单链表的头部添加元素。头插法。"""
-		node = Node(item)
+		node = Node(elem)
 		node.next = self.__head
 		self.__head = node
 
-	def append(self, item):
+	def append(self, elem):
 		"""在单链表的尾部添加元素。尾插法。"""
 
-		node = Node(item)
+		node = Node(elem)
 
 		if self.is_empty():  # 特殊情况
 			self.__head = node
@@ -68,17 +69,17 @@ class SingleLinkList(object):
 				__cur = __cur.next
 			__cur.next = node
 
-	def insert(self, pos, item):
+	def insert(self, pos, elem):
 		"""在单链表的指定位置添加元素。
 
 		:param pos: pos 从0开始
 		"""
 		if pos <= 0:  # 特殊情况
-			self.add(item)
+			self.add(elem)
 		elif pos > (self.length() - 1):  # 特殊情况
-			self.append(item)
+			self.append(elem)
 		else:
-			__pre = self.__head  # Thinking：
+			__pre = self.__head  # 游标
 			count = 0
 
 			while count < (pos - 1):  # Thinking：
@@ -86,23 +87,53 @@ class SingleLinkList(object):
 				__pre = __pre.next
 
 			# 当循环退出后，__pre指向 pos - 1 的位置
-			node = Node(item)
+			node = Node(elem)
 			node.next = __pre.next
 			__pre.next = node
 
-	def remove(self, item):
-		"""删除单链表中的一个元素节点。"""
+	def remove(self, elem):
+		"""删除单链表中的一个数据。 obj.remove(200) 删除掉第一个200这个数据"""
 
-	def search(self, item):
+		__cur = self.__head  # 游标 __cur
+		__pre = None  # 游标 __pre
+
+		while __cur is not None:  # Thinking:循环的结束条件是 __cur.next is not None 还是 __cur is not None?
+			if __cur.element == elem:  # 删除条件
+				# 先判断此节点是否是头节点
+				# 头节点
+				if __cur == self.__head:
+					self.__head = __cur.next
+				else:
+					__pre.next = __cur.next  # __pre.next = __pre.next.next
+				break
+			else:
+				# 先移动__pre： __pre = __cur, __cur = __cur.next
+				__pre = __cur
+				__cur = __cur.next
+
+	def search(self, element):
 		"""查找单链表中某节点元素是否存在。"""
+
+		__cur = self.__head  # 游标
+
+		while __cur is not None:  # Thinking:循环的结束条件是 __cur.next is not None 还是 __cur is not None?
+
+			if __cur.element == element:
+				return True
+			else:
+				__cur = __cur.next
+
+		return False
 
 
 if __name__ == '__main__':
+	# 测试节点
+	node = Node(100)
+	print(node, node.element, node.next)
 
-	# node = Node(100)
-	# print(node, node.element, node.next)
-
+	# 测试单链表
 	single_obj = SingleLinkList()
+
 	print(single_obj.length())  #
 	print(single_obj.is_empty())  #
 
@@ -111,6 +142,7 @@ if __name__ == '__main__':
 	single_obj.append(400)
 
 	single_obj.add(1)  # 头插法
+	single_obj.add(2)
 	single_obj.add(2)
 	single_obj.add(3)
 
@@ -122,3 +154,19 @@ if __name__ == '__main__':
 	single_obj.insert(12, 555)
 
 	single_obj.travel()
+
+	print(single_obj.search(3))
+
+	single_obj.remove(2)
+	single_obj.travel()
+
+	"""测试结果：
+	<__main__.Node object at 0x0000024FC3E212B0> 100 None
+	0
+	True
+	False
+	7
+	333 3 66 2 2 1 200 300 400 555 
+	True
+	333 3 66 2 1 200 300 400 555 
+	"""
